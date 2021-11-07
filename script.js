deb7afac620b583f5043b99998478688a9481956
Luiz-Web <luizhbr03@gmail.com>
2021-11-07 09:58:25 -0300

    Adicionando cálculo de porcentagem

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 function calcular(tipo, valor) {
-    if (tipo ==='acao') {
-        
-        if(valor === 'c'){
-            // limpar o visor
+    if(tipo === 'acao') {
+
+        if(valor == 'c') {
+            //limpart o resultado(id resultado)
             document.getElementById('resultado').value = ''
         }
-        if(valor === '+' || valor === '-' ||  valor === '*' || valor === '/' || valor === '.') {
+        
+        if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
             document.getElementById('resultado').value += valor
         }
-        if (valor === '=') {
+
+        if(valor === '=') {
             var valor_campo = eval(document.getElementById('resultado').value)
             document.getElementById('resultado').value = valor_campo
         }
 
-    } else if (tipo === 'valor'){
-        var valor_campo = document.getElementById('resultado').value
-        document.getElementById('resultado').value += valor
+        if(valor === '%') {
+            var valor_campo = eval(document.getElementById('resultado').value)
+            document.getElementById('resultado').value = valor_campo / 100
+        }
 
+    } else if(tipo === 'valor'){
+        document.getElementById('resultado').value +=  valor
     }
 }
